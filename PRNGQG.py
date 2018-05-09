8c17ebc4f0d8601c287a2cad71015b45950b21bf
Ali Akbar <aliraal12@gmail.com>
2018-05-09 16:24:12 +0700

    Add sequence shuffling algorithm

--- PRNGQG.py
+++ PRNGQG.py
@@ -1,6 +1,7 @@
 """
     Quasigroup Class
 """
+import numpy as np
 
 class PRNGQG():
     def __init__(self, N=256):
@@ -42,21 +43,20 @@ class PRNGQG():
                 self.QG[i][j] = (i + j)%self.N
         self.idx = 0
 
-    def Generate(self, s1, s2, limit):
+    def Generate(self, s1, s2):
         """
             Generate random number using 2 seeds number
             that chose randomly as input.
             limit parameter used to limit the iteration process.
         """
         self.Initialize_Matrix()
-        while (self.idx < limit):
-            rnd = self.QG[s1][s2]
-            s1 = s2
-            s2 = rnd
-            self.ShuffleColumn(self.idx, s1)
-            self.ShuffleRow(self.idx, s2)
-            rnd = self.QG[s1][s2]
-            self.idx = (self.idx+1)%self.N
+        rnd = self.QG[s1][s2]
+        s1 = s2
+        s2 = rnd
+        self.ShuffleColumn(self.idx, s1)
+        self.ShuffleRow(self.idx, s2)
+        rnd = self.QG[s1][s2]
+        self.idx = (self.idx+1)%self.N
         return rnd
 
     def Save_Matrix(self, filename):
@@ -72,9 +72,42 @@ class PRNGQG():
                         f.write("{}".format(self.QG[i][j]))
                 f.write("\n")
 
+    def shuffle(self, seq, key_seed):
+        """
+            Shuffle a sequence using provided seed using Fisher-Yates shuffle
+        """
+        i = len(seq)-1
+        randomizer = PRNGQG(len(seq)-1)
+        while (i > 1):
+            randomizer1 = PRNGQG(i-1)
+            s1, s2 = randomizer1.generate_seed_from_string(key_seed)
+            j = randomizer.Generate(s1, s2)
+            tmp = seq[i]
+            seq[i] = seq[j]
+            seq[j] = tmp
+            i = i-1
+        return seq
+
+    def generate_seed_from_string(self, s):
+        """
+            Generate seeds from string
+        """
+        str1 = s[:(len(s)//2)]
+        str2 = s[len(s)//2:]
+        int1 = sum(map(ord, str1)) % self.N
+        int2 = sum(map(ord, str2)) % self.N
+        return (int1, int2)
+
+
 def main():
-    rnd = PRNGQG().Generate(131, 150, 100)
+    rnd = PRNGQG(11).Generate(2, 8)
     print ("Random number generator : {}".format(rnd))
 
+    # Test shuffle
+    seq = [x for x in range(100)]
+    key = "otista"
+    randomizer = PRNGQG(len(seq))
+    seq = randomizer.shuffle(seq, key)
+    print (seq[:20])
 if __name__ == '__main__':
     main()
