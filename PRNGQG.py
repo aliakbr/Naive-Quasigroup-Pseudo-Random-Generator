0b43313a2021051b4c17af275a9abc04d180d7cb
Ali Akbar <aliraal12@gmail.com>
2018-05-09 19:56:47 +0700

    Fix generator

--- PRNGQG.py
+++ PRNGQG.py
@@ -2,12 +2,18 @@
     Quasigroup Class
 """
 import numpy as np
+import random
+
+SHIFT_CONSTANT = 2
 
 class PRNGQG():
     def __init__(self, N=256):
         self.N = N # Order of the number range
         self.QG = [[0 for x in range(self.N)] for y in range(self.N)]
+        self.gen_matrix = [[0 for x in range(self.N)] for y in range(self.N)]
+        self.init_PRNGQG = False
         self.idx = 0
+        self.init = False
 
     def ShuffleRow(self, i, j):
         """
@@ -43,21 +49,89 @@ class PRNGQG():
                 self.QG[i][j] = (i + j)%self.N
         self.idx = 0
 
-    def Generate(self, s1, s2):
+    def generate_seed_from_string(self, s):
+        """
+            Generate seeds from string
+        """
+        str1 = s[:(len(s)//2)]
+        str2 = s[len(s)//2:]
+        int1 = sum(map(ord, str1)) % self.N
+        int2 = sum(map(ord, str2)) % self.N
+        return (int1, int2)
+
+    def Shuffle_Matrix(self, s1, s2, limit):
         """
             Generate random number using 2 seeds number
             that chose randomly as input.
             limit parameter used to limit the iteration process.
         """
-        self.Initialize_Matrix()
-        rnd = self.QG[s1][s2]
-        s1 = s2
-        s2 = rnd
-        self.ShuffleColumn(self.idx, s1)
-        self.ShuffleRow(self.idx, s2)
-        rnd = self.QG[s1][s2]
-        self.idx = (self.idx+1)%self.N
-        return rnd
+        if (self.init == False):
+            self.Initialize_Matrix()
+            self.init = True
+        for i in range(limit):
+            rnd = self.QG[s1][s2]
+            s1 = s2
+            s2 = rnd
+            self.ShuffleColumn(self.idx, s1)
+            self.ShuffleRow(self.idx, s2)
+            rnd = self.QG[s1][s2]
+            self.idx = (self.idx+1)%self.N
+
+    def LookupOperator(self, table, i, j):
+        return table[i][j]
+
+    def Phase_1(self):
+        if self.init_PRNGQG == True:
+            tmp_matrix = self.QG
+        else:
+            tmp_matrix = self.gen_matrix
+        for i in range(self.N):
+            if (i < self.N-1):
+                for j in range(self.N):
+                    if (j < self.N-1):
+                        self.gen_matrix[i][j] = self.LookupOperator(tmp_matrix, tmp_matrix[i][j], tmp_matrix[i][j+1])
+                    else:
+                        self.gen_matrix[i][j] = self.LookupOperator(tmp_matrix, tmp_matrix[i][j], tmp_matrix[i+1][1])
+            else:
+                for j in range(self.N):
+                    if (j < self.N-1):
+                        self.gen_matrix[i][j] = self.LookupOperator(tmp_matrix, tmp_matrix[i][j], tmp_matrix[i][j+1])
+                    else:
+                        self.gen_matrix[i][j] = self.LookupOperator(tmp_matrix, tmp_matrix[i][j], tmp_matrix[1][1])
+        self.init_PRNGQG = False
+
+    def Phase_2(self):
+        seq = []
+        for i in range(self.N):
+            seq += self.gen_matrix[i]
+        return seq
+
+    def ShiftRow(self, row):
+        new_row = [[] for x in range(self.N)]
+        for i in range(self.N):
+            new_row[i] = row[(i+2) % self.N]
+        return new_row
+
+    def Phase_3(self):
+        for i in range(self.N):
+            self.gen_matrix[i] = self.ShiftRow(self.gen_matrix[i])
+
+    def Generate_Number_Sequence(self, seed_key="haha", limit_shuffle=4, limit=10):
+        """
+            Function to generate a pseudorandom number sequence
+        """
+        seq = []
+        s1, s2 = self.generate_seed_from_string(seed_key)
+        self.Shuffle_Matrix(s1, s2, limit_shuffle)
+        self.init_PRNGQG = True
+        while (len(seq) < limit):
+            self.Phase_1()
+            self.init_PRNGQG = False
+            seq += self.Phase_2()
+            self.Phase_3()
+        if (len(seq) > limit):
+            seq = seq[:limit]
+        return seq
 
     def Save_Matrix(self, filename):
         """
@@ -76,38 +150,30 @@ class PRNGQG():
         """
             Shuffle a sequence using provided seed using Fisher-Yates shuffle
         """
-        i = len(seq)-1
         randomizer = PRNGQG(len(seq)-1)
-        while (i > 1):
-            randomizer1 = PRNGQG(i-1)
-            s1, s2 = randomizer1.generate_seed_from_string(key_seed)
-            j = randomizer.Generate(s1, s2)
+        rand_seq = randomizer.Generate_Number_Sequence(key_seed, limit_shuffle=4, limit=len(seq))
+        for i in range(len(seq)):
             tmp = seq[i]
-            seq[i] = seq[j]
-            seq[j] = tmp
-            i = i-1
+            seq[i] = seq[rand_seq[i]]
+            seq[rand_seq[i]] = tmp
         return seq
 
-    def generate_seed_from_string(self, s):
-        """
-            Generate seeds from string
-        """
-        str1 = s[:(len(s)//2)]
-        str2 = s[len(s)//2:]
-        int1 = sum(map(ord, str1)) % self.N
-        int2 = sum(map(ord, str2)) % self.N
-        return (int1, int2)
-
+    def get_number(self, seed):
+        seq = self.Generate_Number_Sequence(limit=seed+SHIFT_CONSTANT)
+        return seq[seed]
 
 def main():
-    rnd = PRNGQG(11).Generate(2, 8)
+    rnd = PRNGQG(11).get_number(9)
     print ("Random number generator : {}".format(rnd))
 
     # Test shuffle
-    seq = [x for x in range(100)]
+    seq = [x for x in range(20)]
     key = "otista"
     randomizer = PRNGQG(len(seq))
     seq = randomizer.shuffle(seq, key)
-    print (seq[:20])
+    print (seq)
+    seq = randomizer.shuffle(seq, key)
+    print (seq)
+
 if __name__ == '__main__':
     main()
